cb755d1d92a55ac57105fee6d44b7a169447929c
Jonathan <jjstone707@gmail.com>
2018-12-14 00:00:35 +0000

    added db for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,12 @@
 class Place < ApplicationRecord
 	paginates_per 10
+	
 	belongs_to :user
+	has_many :comments
+
 	geocoded_by :address
 	after_validation :geocode
+
 	validates :name, presence: true, length: {minimum: 2}
 	validates :description, presence: true, length: {minimum: 10, maximum:150}
 	validates :address, presence: true, length: {minimum: 10}
